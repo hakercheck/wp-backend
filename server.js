0957c9ded2575cb0fe406c064e5f0208db26182a
hakercheck <93756774+hakercheck@users.noreply.github.com>
2024-11-17 03:03:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors());
 
-const allowedOrigins = ['*'];
+const allowedOrigins = ['https://wp-frontend-080v.onrender.com'];
 
 app.use(cors({
   origin: allowedOrigins,
